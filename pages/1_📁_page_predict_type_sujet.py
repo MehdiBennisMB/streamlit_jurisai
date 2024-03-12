285032362d2ee156c4d4783054c7a5ba0af1da69
othman mesfioui raja <othmanmesfioui@gmail.com>
2024-03-12 17:40:01 +0100

    mise à jour page 1

--- pages/1_📁_page_predict_type_sujet.py
+++ pages/1_📁_page_predict_type_sujet.py
@@ -15,15 +15,36 @@ import os
 # )
 # text=st.text_input("Text to be analysed","Décret")
 
+button1=st.button(label='Load document')
+
+def save_uploaded_file(uploaded_file):
+  with open(os.path.join(os.getcwd(),'document_test'),"wb") as f:
+     f.write(uploaded_file.getbuffer())
+  return st.success("Saved file :{} ".format(uploaded_file.name))
+
+if button1:
+    datafile = st.file_uploader("Upload text",type=['txt'])
+    if datafile is not None:
+        file_details = {"FileName":datafile.name,"FileType":datafile.type}
+        save_uploaded_file(datafile)
+
+
+
 st.markdown('''
 You can classify Moroccan legal documents that you have right here !
 ''')
 
-fichier = open(os.path.join(os.getcwd(),'document_test','TypeArreteSujetSanteFrench0.txt'), "r")
 
+def file_selector(folder_path=os.path.join(os.getcwd(),'document_test')):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    file_path=os.path.join(folder_path, selected_filename)
+    return file_path
+
+
+filename=file_selector()
+fichier = open(os.path.join(os.getcwd(),'document_test',filename), "r")
 text=fichier.read()
-st.markdown(text)
-
 fichier.close()
 
 data = json.dumps(text).encode('utf-8')
@@ -34,7 +55,6 @@ response = requests.post(url, data=data, headers={ 'Content-Type': 'text/plain' 
 prediction=response.json()
 type = prediction['type']
 subject = prediction['subject']
-
 
 st.header(f'Tipe:{type}')
 st.header(f'{subject}')
