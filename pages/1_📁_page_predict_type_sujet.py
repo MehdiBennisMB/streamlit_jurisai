8827b895316805b28310dc83f57c4cde4b7c6174
othman mesfioui raja <othmanmesfioui@gmail.com>
2024-03-13 10:00:23 +0100

    mise à jour page 1 réussite

--- pages/1_📁_page_predict_type_sujet.py
+++ pages/1_📁_page_predict_type_sujet.py
@@ -2,8 +2,7 @@ import streamlit as st
 import requests
 import json
 import os
-
-
+from io import StringIO
 
 '''
 # JurisAI
@@ -15,46 +14,33 @@ import os
 # )
 # text=st.text_input("Text to be analysed","Décret")
 
-button1=st.button(label='Load document')
-
-def save_uploaded_file(uploaded_file):
-  with open(os.path.join(os.getcwd(),'document_test'),"wb") as f:
-     f.write(uploaded_file.getbuffer())
-  return st.success("Saved file :{} ".format(uploaded_file.name))
-
-if button1:
-    datafile = st.file_uploader("Upload text",type=['txt'])
-    if datafile is not None:
-        file_details = {"FileName":datafile.name,"FileType":datafile.type}
-        save_uploaded_file(datafile)
 
 
 
-st.markdown('''
-You can classify Moroccan legal documents that you have right here !
-''')
 
 
-def file_selector(folder_path=os.path.join(os.getcwd(),'document_test')):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    file_path=os.path.join(folder_path, selected_filename)
-    return file_path
+uploaded_file = st.file_uploader("Choose a file")
+print(uploaded_file)
+if uploaded_file is not None:
+    # print(uploaded_file)
+    bytes_data = uploaded_file.getvalue()
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write("You selected the file:", uploaded_file.name)
+    # uploaded_file.read()
+    text = stringio.read()
+    data = json.dumps(text).encode('utf-8')
+    url = "https://jurisaiimage-l6sefij74q-ew.a.run.app/predict"
+    response = requests.post(url, data=data, headers={ 'Content-Type': 'text/plain' })
+    prediction=response.json()
+    type = prediction['type']
+    subject = prediction['subject']
+
+    st.header(f'Type:{type}')
+    st.header(f'{subject}')
 
 
-filename=file_selector()
-fichier = open(os.path.join(os.getcwd(),'document_test',filename), "r")
-text=fichier.read()
-fichier.close()
+# fichier = open(os.path.join(os.getcwd(),'document_test',filename), "r")
+# text=fichier.read()
+# fichier.close()
 
-data = json.dumps(text).encode('utf-8')
-
-url = "https://jurisaiimage-l6sefij74q-ew.a.run.app/predict"
-response = requests.post(url, data=data, headers={ 'Content-Type': 'text/plain' })
-
-prediction=response.json()
-type = prediction['type']
-subject = prediction['subject']
-
-st.header(f'Tipe:{type}')
-st.header(f'{subject}')
+# data = json.dumps(text).encode('utf-8')
